dca17c81de15095957dd523af62495601669360d
meepling123 <meepling123@yahoo.com>
2021-03-24 19:27:55 -0700

    adding a company class and an additional view meant to edit employeeProfile.class

---
+++ app/src/main/java/com/example/jobsandtasks/company.java
@@ -0,0 +1,14 @@
+package com.example.jobsandtasks;
+
+public class company
+{
+    public company(String name, String Country, String postalCode, String region, String description)
+    {
+
+    }
+
+    public void postJob()
+    {
+
+    }
+}
